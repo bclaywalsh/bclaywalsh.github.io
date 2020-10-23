8a58d7933dd6666e04c30ddce1c087772cb5d5de
bclaywalsh <bclaywalsh@gmail.com>
2020-10-22 23:09:30 -0400

    added some title page styling, needs to be randomized

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ function hamburgerMenu () {
 // Slideshow Switch Subject
 const reviewsSlideshow = document.querySelector(".slideshow-container-reviews");
 const derelictStarSlideshow = document.querySelector(".slideshow-container-derelict-star");
+const gardenSlideshow = document.querySelector(".slideshow-container-garden");
 
-const slideShows = [reviewsSlideshow, derelictStarSlideshow];
+const slideShows = [reviewsSlideshow, derelictStarSlideshow, gardenSlideshow];
 
 const reviewsButton = document.getElementById("reviewsButton");
 const derelictStarButton = document.getElementById("derelictStarButton");
+const gardenButton = document.getElementById("petsAmokButton");
+
 
 
 derelictStarButton.addEventListener('click', openDerelictStarSlides);
 reviewsButton.addEventListener('click', openReviewsSlides);
+gardenButton.addEventListener('click', openGardenSlides);
 
 
 function openDerelictStarSlides(){
     let x;
-    // showSlides(slideIndex);
-
     for(x of slideShows) {
         x.style.display="none";
     }
@@ -34,15 +36,19 @@ function openDerelictStarSlides(){
 
 function openReviewsSlides(){
     let x;
-    // showSlides(slideIndex);
-
     for(x of slideShows) {
         x.style.display="none";
     }
     reviewsSlideshow.style.display="block";
 }
 
-
+function openGardenSlides(){
+    let x;
+    for(x of slideShows) {
+        x.style.display="none";
+    }
+    gardenSlideshow.style.display="block";
+}
 
 // Slideshow
 var slideIndex = 1;
@@ -58,15 +64,10 @@ function currentSlide(i) {
 
 function showSlides(n){
     var s;
-    // var slides = document.getElementsByClassName("mySlides");
-
     var dSSlides = document.getElementsByClassName("myDSSlides");
     var revSlides = document.getElementsByClassName("myRevSlides");
-
+    var gardenSlides = document.getElementsByClassName("myGardenSlides");
     var dots = document.getElementsByClassName("dot");
-
-    // if (n > slides.length) {slideIndex=1}
-    // if (n < 1) {slideIndex = slides.length}
 
     if (n > dSSlides.length) {slideIndex=1}
     if (n < 1) {slideIndex = dSSlides.length}
@@ -74,19 +75,19 @@ function showSlides(n){
     if (n > revSlides.length) {slideIndex=1}
     if (n < 1) {slideIndex = revSlides.length}
 
-    // for (s=0; s<slides.length; s++) {
-    //     slides[s].style.display = "none";
-    // }
-    // for (s=0; s<dots.length; s++) {
-    //     dots[s].className = dots[s].className.replace(" active", "");
-    // }
+    if (n > gardenSlides.length) {slideIndex=1}
+    if (n < 1) {slideIndex = gardenSlides.length}
+
+
     for (s=0; s<dSSlides.length; s++) {
         dSSlides[s].style.display = "none";
     }
     for (s=0; s<revSlides.length; s++) {
         revSlides[s].style.display = "none";
     }
-
+    for (s=0; s<gardenSlides.length; s++) {
+        gardenSlides[s].style.display = "none";
+    }
 
     for (s=0; s<dots.length; s++) {
         dots[s].className = dots[s].className.replace(" active", "");
@@ -94,6 +95,8 @@ function showSlides(n){
     // slides[slideIndex-1].style.display = "block";
     dSSlides[slideIndex-1].style.display = "block";
     revSlides[slideIndex-1].style.display = "block";
+    gardenSlides[slideIndex-1].style.display = "block";
+
     dots[slideIndex-1].className += " active";
 }
 
@@ -165,52 +168,58 @@ for (sp of modals){
 
 
 // ANIMATED BOXES
-// const animatedBoxesLayer = document.querySelector(".animatedBoxes");
-// const box1 = document.querySelector("#animatedBox1");
-// const box2 = document.querySelector("#animatedBox2");
-// const box3 = document.querySelector("#animatedBox3");
-// const box4 = document.querySelector("#animatedBox4");
-// const box5 = document.querySelector("#animatedBox5");
-// const box6 = document.querySelector("#animatedBox6");
+const animatedBoxesLayer = document.querySelector(".animatedBoxes");
+const box1 = document.querySelector("#animatedBox1");
+const box2 = document.querySelector("#animatedBox2");
+const box3 = document.querySelector("#animatedBox3");
+const box4 = document.querySelector("#animatedBox4");
+const box5 = document.querySelector("#animatedBox5");
+const box6 = document.querySelector("#animatedBox6");
+const myMove = css.myMove;
+const movingBoxes = [box1, box2, box3, box4, box5, box6];
 
-// const movingBoxes = [box1, box2, box3, box4, box5, box6];
-
-// const viewWidth = window.innerWidth;
-// const viewHeight = window.innerHeight;
+const viewWidth = window.innerWidth;
+const viewHeight = window.innerHeight;
 
 //Initialize Attributes
-// let x;
-// for (x of movingBoxes){
-//     x.defineProperty(x, 'pos-x', { value: (viewWidth + (Math.random()-0.5)*300)});
-//     Object.defineProperty(x, 'pos-y', { value: (viewHeight + (Math.random()-0.5)*300)});
-//     Object.defineProperty(x, 'scale', { value: (Math.random()*100)});
-//     Object.defineProperty(x, 'x-scale', { value: (x.scale * Math.random())});
-//     Object.defineProperty(x, 'payne-alpha', { value: (x.x-scale)});
-//     Object.defineProperty(x, 'frame-alpha', { value: (1-x.x-scale)});
+// let mb;
+// for (mb of movingBoxes){
+//     object.defineProperty(mb, 'pos_x', { value: (viewWidth + (Math.random()-0.5)*300)});
+//     Object.defineProperty(mb, 'pos_y', { value: (viewHeight + (Math.random()-0.5)*300)});
+//     Object.defineProperty(mb, 'scale', { value: (Math.random()*100)});
+//     Object.defineProperty(mb, 'x_scale', { value: (mb.scale * Math.random())});
+//     Object.defineProperty(mb, 'payne_alpha', { value: (mb.x-scale)});
+//     Object.defineProperty(mb, 'frame_alpha', { value: (1-mb.x-scale)});
 
 
-    // x[pos-y] = viewHeight + (Math.random()-0.5)*300;
-    // x[scale] = Math.random()*100;
-    // x[x-scale] = x.scale * Math.random();
-    // x[payne-alpha] = x.x-scale;
-    // x[frame-alpha] = 1-x.x-scale;
+//     x[pos-y] = viewHeight + (Math.random()-0.5)*300;
+//     x[scale] = Math.random()*100;
+//     x[x-scale] = x.scale * Math.random();
+//     x[payne-alpha] = x.x-scale;
+//     x[frame-alpha] = 1-x.x-scale;
 // }
 
-//Move Boxes
+// Move Boxes
 // function moveMyBoxes(){
-//     let x;
-//     for (x of movingBoxes){
-//         var id = setInterval(frame, 5);
-//         function frame() {
-//             if (pos == 350) {
-//             clearInterval(id);
-//             } else {
-//             pos++; 
-//             elem.style.top = pos + "px"; 
-//             elem.style.left = pos + "px"; 
-//             }
-//         }
+
+// for (let i=1; i<6; i++){
+//     setTimeout(frame, 10*i, i){
 //     }
+// }
+
+// function frame(i) {
+//     box1.pos_x-=1;
+//     box1.pos_y-=1;
+//     box1.style.top = b.pos-y + "px"; 
+//     box1.style.left = b.pos-x + "px"; 
+//     // if (box1.pos-y < -100) {
+//     //     box1.pos-y = viewHeight + (Math.random()-0.5)*300;
+//     //     box1.pos-x = viewWidth + (Math.random()-0.5)*300;
+//     // } else {
+//     //     box1.style.top = b.pos-y + "px"; 
+//     //     box1.style.left = b.pos-x + "px"; 
+//     // }
+// }
 // }
 
 // const movingBox1 = {id: 1, x : 1200, y :1000, x_scale : 1, y_scale : 1, alpha : 0.5, zoomDir : 1};
@@ -239,17 +248,26 @@ for (sp of modals){
 //     }
 // }
 
-// function myMove() {
-//     var elem = document.getElementById("animate");   
-//     var pos = 0;
-//     var id = setInterval(frame, 5);
-//     function frame() {
-//       if (pos == 350) {
-//         clearInterval(id);
-//       } else {
-//         pos++; 
-//         elem.style.top = pos + "px"; 
-//         elem.style.left = pos + "px"; 
-//       }
-//     }
-//   }
+myMove();
+function myMove() {
+    let mb;
+    for(mb of movingBoxes){
+        mb.style.left = parseInt(mb.style.left)-1+"px";
+        mb.style.top = parseInt(mb.style.top)-1+"px";
+        if(mb.style.left<-100) mb.style.left=viewWidth;
+        if(mb.style.top<-100) mb.style.top=viewHeight;
+    }
+    
+    // var elem = document.getElementsByName("movingBox");   
+    // var pos = 0;
+    // var id = setInterval(frame, 5);
+    // function frame() {
+    //   if (pos == 350) {
+    //     clearInterval(id);
+    //   } else {
+    //     pos++; 
+    //     elem.style.top = pos + "px"; 
+    //     elem.style.left = pos + "px"; 
+    //   }
+    // }
+  }
